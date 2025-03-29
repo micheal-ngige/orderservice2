173ad893cbd8bc427def4b1d7034d60696574b44
michael <michaelngige@gmail.com>
2025-03-29 13:02:25 +0300

    Added home route

--- backend/orderservice/urls.py
+++ backend/orderservice/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the Order Service API")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), 
     path('orders/', include('orders.urls')),
     path('customers/', include('customers.urls')),
+    path('', home),  
 ]
+
